fe70e62540ca49052600e201d2cec92d058ad991
Milton2332 <daniel1hodrick@gmail.com>
2023-10-31 15:42:14 -0400

    creacion de laravel

---
+++ resources/views/lol.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+
+        <style>
+            .fondo{
+                background-image:url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
+                background-position: center;
+                --tw-bg-opacity:1;
+                background-color:rgb(243 244 246 / var(--tw-bg-opacity));
+                min-height:100vh;
+                position:relative;
+                --tw-bg-opacity:1;background-color:rgb(17 24 39 / var(--tw-bg-opacity))
+
+            }
+
+        </style>
+
+    </head>
+    <body class="">
+
+        <div class="fondo ">
+
+
+
+        </div>
+    </body>
+</html>
